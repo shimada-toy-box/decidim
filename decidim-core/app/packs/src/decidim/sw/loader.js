50abc50561b1d4fc93ec2e347c8a64aabf10c502
quino <quino@populate.tools>
2022-02-02 16:54:14 +0100

    Add send push notification service

--- decidim-core/app/packs/src/decidim/sw/loader.js
+++ decidim-core/app/packs/src/decidim/sw/loader.js
@@ -1,8 +1,18 @@
 // check if the browser supports serviceWorker at all
 window.addEventListener("load", async () => {
   if ("serviceWorker" in navigator) {
-    // eslint-disable-next-line no-unused-vars
+    const vapidPublicKeyElement = JSON.parse(document.querySelector("#vapidPublicKey").value)
     const registration = await navigator.serviceWorker.register("/sw.js", { scope: "/" });
+    const subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: new Uint8Array(vapidPublicKeyElement)
+    });
+
+    await fetch("/subscribe_to_notifications", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(subscription)
+    });
   } else {
     console.log("Your browser doesn't support service workers 🤷‍♀️");
   }
